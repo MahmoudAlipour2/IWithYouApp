2d09ace7806b2e0ac1a1388340e2786d81904206
unknown <ostovaar@gmail.com>
2019-05-13 02:55:48 +0430

    avalin gereftane ok ba ersale json bt service

---
+++ app/src/main/java/ir/iwithyou/app/register/model/SendModelForRegister.java
@@ -0,0 +1,11 @@
+package ir.iwithyou.app.register.model;
+
+public class Register {
+    {
+        String Email;
+        String Password;
+        String FirstName;
+        String LastName;
+        String PhoneNumber;
+    }
+}
