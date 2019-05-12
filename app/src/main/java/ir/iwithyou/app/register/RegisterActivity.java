964a8d2e345d0f9b63cb81dd6388e908351ed040
unknown <ostovaar@gmail.com>
2019-05-12 14:45:08 +0430

    Initial commit

---
+++ app/src/main/java/ir/iwithyou/app/register/RegisterActivity.java
@@ -0,0 +1,14 @@
+package ir.iwithyou.app.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ir.iwithyou.app.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
