d837d4bdfd10ac625e74331b67e9f9671c6ab356
unknown <ostovaar@gmail.com>
2019-05-20 13:31:29 +0430

    design of MainMenu Activity completed-1

---
+++ app/src/main/java/ir/iwithyou/app/features/Menu/View/MainMenuActivity.java
@@ -0,0 +1,17 @@
+package ir.iwithyou.app.features.Menu.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.iwithyou.app.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+
+    }
+}
