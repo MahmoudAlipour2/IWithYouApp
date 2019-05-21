60537f42b15cffb62f1b7df89130d529e956370f
unknown <ostovaar@gmail.com>
2019-05-21 20:53:17 +0430

    Camera Opened.

--- app/src/main/java/ir/iwithyou/app/features/Menu/View/MainMenuActivity.java
+++ app/src/main/java/ir/iwithyou/app/features/Menu/View/MainMenuActivity.java
@@ -1,17 +1,52 @@
 package ir.iwithyou.app.features.Menu.View;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ir.iwithyou.app.MainActivity;
 import ir.iwithyou.app.R;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    public static final int REQUEST_TAKE_PHOTO = 1;
+    //public static final int REQUEST_TAKE_VIDEO = 1;
+    public static final int REQUEST_PICK_PHOTO = 2;
+    public static final int REQUEST_PICK_VIDEO = 3;
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    public static final int MEDIA_TYPE_VIDEO = 5;
+    private Uri mMediaUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ButterKnife.bind(this);
+
 
 
     }
+
+
+
+
+    @OnClick(R.id.itvicon)
+    public void takePhoto() {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePhotoIntent,REQUEST_TAKE_PHOTO);
+    }
+
 }
